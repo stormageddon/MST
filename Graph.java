4c5589168cd35200e6d17483ae2428ede561033e
Michael Caputo <therubikscubekid@gmail.com>
2014-02-15 21:03:23 -0500

    Added regeneration of graph if graph was not connected.

--- Graph.java
+++ Graph.java
@@ -7,20 +7,17 @@ import java.util.*;
 
 public class Graph {
     private Map<Integer, Vertex> adjacencyList;
-    private Random vertexGenerator = new Random();
     private Random weightGenerator = new Random(200000);
     private Random connectionGenerator = new Random(100000);
-    private List<Integer> remainingVertices = new ArrayList<Integer>();
     private int n, seed;
     private double p;
 
     public Graph(int n, int seed, double p) {
-	System.out.println("Constructing a graph");
 	adjacencyList = new HashMap<Integer, Vertex>();
 	this.n = n;
 	this.seed = seed;
 	this.p = p;
-	generateGraph();
+	//	generateGraph();
     }
 
     /*
@@ -30,7 +27,6 @@ public class Graph {
 
 	for (int i = 0; i < n; i++ ) {
 	    addVertex(i);
-	    //	    remainingVertices.add(new Integer(i));
 	}
 	for (int i = 0; i < adjacencyList.size(); i++) {
 	    addNeighbors(i);
@@ -45,9 +41,8 @@ public class Graph {
     public void addNeighbors(int vertexId) {
 	for( int element = vertexId; element < adjacencyList.size(); element++ ) {
 	    if( element != vertexId ) {
-		if (connectionGenerator.nextDouble() <= 0.5) {
-		    //		    adjacencyList.get(vertexId).add(element);
-		    int weight = weightGenerator.nextInt(7) + 1;
+		if (connectionGenerator.nextDouble() <= p) {
+		    int weight = weightGenerator.nextInt(n) + 1;
 		    adjacencyList.get(vertexId).addNeighbor(adjacencyList.get(element),new Integer(weight));
 		    adjacencyList.get(element).addNeighbor(adjacencyList.get(vertexId),new Integer(weight));
 		}
@@ -65,11 +60,11 @@ public class Graph {
 		System.out.printf(" %d(%d)",((Vertex)currNeighbor.vertex).getId(), currNeighbor.weight);
 	    }
 	}
-	System.out.println("");
+	System.out.println("\n");
     }
 
     public void printGraphAsMatrix() {
-	System.out.println("The graph as an adjacency matrix:");
+	System.out.println("The graph as an adjacency matrix:\n");
 	for(int i = 0; i < adjacencyList.size(); i++) {
 	    Vertex currVertex = adjacencyList.get(i);
 	    for(int k = 0; k < adjacencyList.size(); k++) {
@@ -79,7 +74,15 @@ public class Graph {
 		}
 		System.out.printf("%d\t",w);
 	    }
-	    System.out.println("");
+	    System.out.println("\n");
 	}
+    }
+
+    public Vertex getVertex(int vertexId) {
+	return adjacencyList.get(vertexId);
+    }
+
+    public void clear() {
+	adjacencyList = new HashMap<Integer, Vertex>();
     }
 }
